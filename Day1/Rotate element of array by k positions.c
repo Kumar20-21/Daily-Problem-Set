6f33522428f5e4a382e42e96778657449711aab7
Kumar20-21 <63784975+Kumar20-21@users.noreply.github.com>
2021-12-16 01:40:32 +0530

    Update Rotate element of array by k positions.c

--- Day1/Rotate element of array by k positions.c
+++ Day1/Rotate element of array by k positions.c
@@ -1,3 +1,10 @@
+/*
+Method 1:
+Rotation of each element one by one.
+Time complexity: O(n*d)
+Auxiliary space: O(1)
+*/
+
 #include <stdio.h>
 
 int main()
@@ -23,7 +30,7 @@ int main()
         }
         return 0;
     }
-    if (rotation_type == 'C')
+    if (rotation_type == 'A')
     {
         rotate = n - rotate;
     }
@@ -42,3 +49,66 @@ int main()
     }
     return 0;
 }
+
+
+
+/*
+
+Method 2:
+Juggling Method
+Assumption: d < n
+Time complexity: O(n)
+Auxiliary space: O(1)
+
+#include <stdio.h>
+
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+}
+
+//Function to get gcd of a and b
+int gcd(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+//Function to left rotate arr[] of siz n by d
+void leftRotate(int arr[], int d, int n)
+{
+	int i, j, k, temp;
+	// To handle if d >= n
+	d = d % n;
+	int g_c_d = gcd(d, n);
+	for (i = 0; i < g_c_d; i++) {
+		// move i-th values of blocks
+		temp = arr[i];
+		j = i;
+		while (1) {
+			k = j + d;
+			if (k >= n)
+				k = k - n;
+			if (k == i)
+				break;
+			arr[j] = arr[k];
+			j = k;
+		}
+		arr[j] = temp;
+	}
+}
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+	leftRotate(arr, 2, 7);
+	printArray(arr, 7);
+	getchar();
+	return 0;
+}
+
+*/
